e77551541289094592bc625465797b1a2748af09
GradusovRoman <monomari86@gmail.com>
2020-12-12 11:30:50 +0500

    lesson 3_6 try #1

---
+++ src/test/java/ru/geekbrains/ArrayUtilsTest.java
@@ -0,0 +1,83 @@
+package ru.geekbrains;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
+
+
+public class ArrayUtilsTest {
+
+    @ParameterizedTest
+    @MethodSource("expectedParametersProviderForGetArrayAfterDigit")
+    void getArrayAfterDigitGoodResult(int[] parameters, int[] expected) {
+        Assertions.assertArrayEquals(expected, ArrayUtils.findElementsAfterDigit(parameters, 4));
+    }
+
+    private static Stream<Arguments> expectedParametersProviderForGetArrayAfterDigit() {
+        return Stream.of(
+                Arguments.arguments(new int[]{1, 2, 3, 4, 5}, new int[]{5}),
+                Arguments.arguments(new int[]{4, 2, 4, 3}, new int[]{2}),
+                Arguments.arguments(new int[]{1, 2, 3, 4, 4}, new int[]{}),
+                Arguments.arguments(new int[]{4, 1, 1, 1, 1}, new int[]{1, 1, 1, 1}),
+                Arguments.arguments(new int[]{-1, -2, 4, -1, -5}, new int[]{-1, -5})
+        );
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @MethodSource("wrongParametersProviderForGetArrayAfterDigit")
+    void getArrayAfterDigitThrowException(int[] parameters) {
+        RuntimeException e = Assertions.assertThrows(RuntimeException.class,
+                () -> ArrayUtils.findElementsAfterDigit(parameters, 4));
+        Assertions.assertNotNull(e);
+    }
+
+    private static Stream<int[]> wrongParametersProviderForGetArrayAfterDigit() {
+        return Stream.of(
+                new int[]{0, -1, 0, 0},
+                new int[]{},
+                null
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("expectedParametersProviderForCheckDigitsInArray")
+    void shouldReturnTrueCheckDigitsInArray(int[] parameters) {
+        Assertions.assertTrue(ArrayUtils.checkDigitsInArray(parameters, 1, 4));
+    }
+
+    private static Stream<int[]> expectedParametersProviderForCheckDigitsInArray() {
+        return Stream.of(
+                new int[]{1, 4, 1, 4},
+                new int[]{4, 1, 4, 1},
+                new int[]{1, 1, 1, 4, 4},
+                new int[]{4, 4, 1, 1}
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("wrongParametersProviderForCheckDigitsInArray")
+    void shouldReturnFalseCheckDigitsInArray(int[] parameters) {
+        Assertions.assertFalse(ArrayUtils.checkDigitsInArray(parameters, 1, 4));
+    }
+
+    private static Stream<int[]> wrongParametersProviderForCheckDigitsInArray() {
+        return Stream.of(
+                new int[]{1, 4, 1, 2},
+                new int[]{4, 4, 4, 4},
+                new int[]{1, 1, 1, 1, 1},
+                new int[]{1,3,5,2},
+                new int[]{1},
+                new int[]{4},
+                new int[]{},
+                null
+        );
+    }
+
+
+}
